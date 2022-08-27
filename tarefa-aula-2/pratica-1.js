452730b35ea39cd604d1f8ea071035f7f31a4bed
leticia <leticianacaratto6@gmail.com>
2022-08-27 14:58:24 -0300

    pratica 1.js

--- tarefa-aula-2/pratica-1.js
+++ tarefa-aula-2/pratica-1.js
@@ -3,3 +3,4 @@ let email = "leticianacaratto6@gmail.com"
 let profissao = "futura programadora"
 let idade = " 16"
 let lugar = "coreia do sul "
+'Olá,${nome}!, Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D'
