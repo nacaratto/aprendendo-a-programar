009c534071ff3e02abcb4df990a8b6a270acba1a
leticia <leticianacaratto6@gmail.com>
2022-08-28 16:42:47 -0300

    calculadora aula3

---
+++ tarefa-aula-3/pratica-2.js
@@ -0,0 +1,11 @@
+let multiplicador = 10
+let n = 10
+let i = 1
+
+while ( i <= n ){
+    let calculadora = multiplicador * i 
+    let textodeexibicao = `${ multiplicador} X ${i} = ${calculadora }`
+    console.log(textodeexibicao)
+    i++
+}
+
