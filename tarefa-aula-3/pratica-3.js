64e94ea8ef871b75b618888302db353c5d2b59dd
leticia <leticianacaratto6@gmail.com>
2022-08-30 22:09:06 -0300

    exibindo impares aula3

---
+++ tarefa-aula-3/pratica-3.js
@@ -0,0 +1,15 @@
+function exibeimPar(n1,n2){
+    var lista = [
+        ]  
+        while(n1<=n2){
+            if(n1%2!=0){
+            lista.push(n1)
+            }
+        n1++    
+        }
+    return lista
+    }
+
+   console.log(exibeimPar(0,16))
+        { counter= 1}
+        
